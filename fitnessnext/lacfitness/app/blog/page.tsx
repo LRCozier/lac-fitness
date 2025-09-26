cc75e8f07f590e1ba49c348b102b9b9bc0f40379
LRCozier <lrcozier@gmail.com>
2025-09-26 11:56:12 +0100

    updated global css and blog page

---
+++ fitnessnext/lacfitness/app/blog/page.tsx
@@ -0,0 +1,17 @@
+const Blog = () => {
+  return (
+    <div className="section animate-fade-in">
+      <div className="container">
+        <div className="text-center">
+          <h1 className="page-title">THE STRENGTH BLOG</h1>
+          <p className="section-text">Knowledge is power. Here's your weekly dose.</p>
+        </div>
+        <div className="blog-grid">
+          {/* ADD CMS MAPPING THROUGH GRAPH QL API */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;

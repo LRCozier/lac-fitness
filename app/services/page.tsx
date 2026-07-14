e7aafca6d8c4cb7e0b5fdf575a0747e3016cd8cf
LRCozier <lrcozier@gmail.com>
2026-07-14 09:24:52 +0100

    updated services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import ServiceTabs from '@/components/ui/Servicetabs';
+import ServiceTabs from '@/components/ui/Servicestabs';
 import { getServices } from '@/lib/server/Hygraph';
 
 export const metadata: Metadata = {
